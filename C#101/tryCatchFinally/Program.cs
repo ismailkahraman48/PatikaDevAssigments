0dfbd35c3954063688a62d6c6ff20e72af276bd4
İsmail Kahraman <76534257+ismailkahraman48@users.noreply.github.com>
2022-09-28 12:35:19 +0300

    Add files via upload

---
+++ C#101/tryCatchFinally/Program.cs
@@ -0,0 +1,43 @@
+﻿
+#region First Try Catch
+
+// try
+// {
+    
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine(a);
+//     Console.ReadKey();
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine("Error" + ex.Message);
+    
+// }
+// finally{
+//     Console.WriteLine("Program has completed");
+// }
+#endregion
+
+#region Second Try Catch
+
+try
+{
+    string numAsText = Console.ReadLine(); // read line waiting a number as a text in console.
+    int b = int.Parse(numAsText); // parse is converting string to int.
+    Console.WriteLine("İnput: {0} and output: {1}",numAsText,b+120);
+    
+    
+}
+catch (ArgumentNullException ex)
+{
+    
+    Console.WriteLine("You've entred a null value");
+    Console.WriteLine(ex);
+}
+finally{
+    Console.WriteLine("Program's ended.");
+}
+
+
+
+#endregion
